7ca6300b59ef213775281692114ef1395f2a78ee
Kalungibrightangel <b.kalungi@alustudent.com>
2026-05-13 14:26:07 -0700

    jason attrr

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Module that defines a Student class."""
+
+
+class Student:
+    """Define a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the instance."""
+        if type(attrs) is list:
+            new_dict = {}
+
+            for item in attrs:
+                if type(item) is str and hasattr(self, item):
+                    new_dict[item] = getattr(self, item)
+
+            return new_dict
+
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the instance."""
+        for key, value in json.items():
+            setattr(self, key, value)
