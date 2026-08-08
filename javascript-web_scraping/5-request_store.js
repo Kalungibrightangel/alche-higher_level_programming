ba5173b1d46db2880fa7612545adc3abb2998096
Your Name <b.kalungi@alustudent.com>
2026-08-08 08:39:06 -0700

    New js file added

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
