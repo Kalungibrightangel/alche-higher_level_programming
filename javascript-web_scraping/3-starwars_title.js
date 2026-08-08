e20d62f3ad7f19be43307bc9a57246e7e0934624
Your Name <b.kalungi@alustudent.com>
2026-08-08 08:33:03 -0700

    New js file added

---
+++ javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+
+  const movie = JSON.parse(body);
+  console.log(movie.title);
+});
